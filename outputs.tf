9e56a442c47a161e210b410e8ada874a4838925f
esaenz@mcclatchyservices.com <esaenz@mcclatchyservices.com>
2022-05-26 18:35:51 -0500

    Fixing outputs error

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,6 @@ output service_account_prod_id {
     value = google_service_account.service_account_prod.email
 }
 
-output service_account_nonprod_id {
+output service_account_build_id {
     value = google_service_account.service_account_build.email
 }
