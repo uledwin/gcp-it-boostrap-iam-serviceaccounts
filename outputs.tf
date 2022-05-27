b08f9e584e8565ae4e931356fa7f50462a72c445
esaenz@mcclatchyservices.com <esaenz@mcclatchyservices.com>
2022-05-26 19:05:18 -0500

    Changing outputs values for being used in other layers

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output service_account_nonprod_id {
-    value = google_service_account.service_account_nonprod.email
+    value = google_service_account.service_account_nonprod.id
 }
 
 output service_account_prod_id {
-    value = google_service_account.service_account_prod.email
+    value = google_service_account.service_account_prod.id
 }
 
 output service_account_build_id {
-    value = google_service_account.service_account_build.email
+    value = google_service_account.service_account_build.id
 }
