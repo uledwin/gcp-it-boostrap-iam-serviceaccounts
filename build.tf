6be21c10aa431cb6f92b25234a0ce42a828c4957
esaenz@mcclatchyservices.com <esaenz@mcclatchyservices.com>
2022-06-01 08:34:59 -0500

    Adding permissions for reading state files

--- build.tf
+++ build.tf
@@ -4,6 +4,12 @@ resource "google_service_account" "service_account_build" {
   display_name = "Terraform Service Account"
 }
 
+resource "google_service_account" "service_account_nonprod" {
+  project      = var.build_project_id
+  account_id   = "sa-iac-terraform-nonprod"
+  display_name = "Terraform Service Account"
+}
+
 resource "google_project_iam_binding" "project_iam_binding_build_editor" {
   project = var.build_project_id
   role    = "roles/editor"
@@ -13,18 +19,31 @@ resource "google_project_iam_binding" "project_iam_binding_build_editor" {
 }
 
 
-resource "google_service_account" "service_account_nonprod" {
-  project      = var.build_project_id
-  account_id   = "sa-iac-terraform-nonprod"
-  display_name = "Terraform Service Account"
-}
-
-
 resource "google_service_account" "service_account_prod" {
   project      = var.build_project_id
   account_id   = "sa-iac-terraform-prod"
   display_name = "Terraform Service Account"
 }
+
+resource "google_project_iam_binding" "project_iam_binding_build_logwriter" {
+  project = var.build_project_id
+  role    = "roles/logging.logWriter"
+  members = [
+    "serviceAccount:${google_service_account.service_account_nonprod.email}",
+    "serviceAccount:${google_service_account.service_account_prod.email}",
+  ]
+}
+
+resource "google_project_iam_binding" "project_iam_binding_build_storageadmin" {
+  project = var.build_project_id
+  role    = "roles/storage.objectAdmin"
+  members = [
+    "serviceAccount:${google_service_account.service_account_build.email}",
+    "serviceAccount:${google_service_account.service_account_nonprod.email}",
+    "serviceAccount:${google_service_account.service_account_prod.email}",
+  ]
+}
+
 
 resource "google_project_iam_binding" "project_iam_binding_env_editor" {
   project = var.environments_project_id
@@ -37,12 +56,3 @@ resource "google_project_iam_binding" "project_iam_binding_env_editor" {
 }
 
 
-resource "google_project_iam_binding" "project_iam_binding_build_logwriter" {
-  project = var.build_project_id
-  role    = "roles/logging.logWriter"
-  members = [
-    "serviceAccount:${google_service_account.service_account_build.email}",
-    "serviceAccount:${google_service_account.service_account_nonprod.email}",
-    "serviceAccount:${google_service_account.service_account_prod.email}",
-  ]
-}
