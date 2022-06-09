3ee5fe71185d9da8d3373a212fbd699f4eda361c
esaenz@mcclatchyservices.com <esaenz@mcclatchyservices.com>
2022-06-08 19:14:43 -0500

    Adding indexes

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "service_account_nonprod_id" {
-  value = google_service_account.service_account_nonprod.id
+  value = google_service_account.service_account_nonprod[0].id
 }
 
 output "service_account_prod_id" {
-  value = google_service_account.service_account_prod.id
+  value = google_service_account.service_account_prod[0].id
 }
 
 output "service_account_build_id" {
-  value = google_service_account.service_account_build.id
+  value = google_service_account.service_account_build[0].id
 }
