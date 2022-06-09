18db75135dbbe926efadef852a3f1e57f4fc617c
esaenz@mcclatchyservices.com <esaenz@mcclatchyservices.com>
2022-06-08 19:55:30 -0500

    Adding right outputs values

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "service_account_nonprod_id" {
-  value = google_service_account.service_account_nonprod[0].id
+  value = google_service_account.service_account_nonprod.*.id
 }
 
 output "service_account_prod_id" {
-  value = google_service_account.service_account_prod[0].id
+  value = google_service_account.service_account_prod.*.id
 }
 
 output "service_account_build_id" {
-  value = google_service_account.service_account_build[0].id
+  value = google_service_account.service_account_build.*.id
 }
